b222e59ff4cb089bdbda5ac5836d50fc2b045191
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2024-05-24 09:18:40 +0000

    Added api route.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -27,8 +27,13 @@ JOBS = [{
 
 @app.route("/")
 def home_page():
-  return render_template('home.html', jobs=JOBS, company_name='JobFinder')
+    return render_template('home.html', jobs=JOBS, company_name='JobFinder')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
